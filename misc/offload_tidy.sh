82fea89994486a4b0b509b517b6491f1ae378105
Xand Meaden <xand.meaden@kcl.ac.uk>
2025-07-11 00:10:49 +0100

    fix: did I trust copilot?

--- misc/offload_tidy.sh
+++ misc/offload_tidy.sh
@@ -4,9 +4,9 @@
 
 source /etc/systemd/ont-platform-data-offload.conf
 
-if [ -z "${ONT_PLATFORM_DATA_OFFLOAD_DIR}" ]; then
-    echo "ONT_PLATFORM_DATA_OFFLOAD_DIR is not set. Exiting." 1>&2
+if [ -z "${SOURCE_DIR}" ]; then
+    echo "SOURCE_DIR is not set. Exiting." 1>&2
     exit 1
 fi
 
-/usr/bin/find "${ONT_PLATFORM_DATA_OFFLOAD_DIR}" -type d -mtime +30 | /usr/bin/sort -r | /usr/bin/xargs rmdir -v
+/usr/bin/find "${SOURCE_DIR}" -type d -mtime +30 | /usr/bin/sort -r | /usr/bin/xargs rmdir -v
